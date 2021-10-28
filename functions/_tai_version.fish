33dca831953bc7a0577e9f17f1ef7b9facab6145
kobanyan <kobanyan@users.noreply.github.com>
2021-10-28 12:37:56 +0900

    use command instead of which

--- functions/_tai_version.fish
+++ functions/_tai_version.fish
@@ -1,7 +1,7 @@
 function _tai_version -a tai_lang tai_lang_ver
   set -q tai_asdf_indicator; or set -g tai_asdf_indicator ''
   set -q tai_system_indicator; or set -g tai_system_indicator '*'
-  which "$tai_lang" | string match -q "$HOME/.asdf/*";
+  command -s "$tai_lang" | string match -q "$HOME/.asdf/*";
     and echo "$tai_lang_ver$tai_asdf_indicator";
     or echo "$tai_lang_ver$tai_system_indicator"
 end
