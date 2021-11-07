aa5279a37e6968fb829134e0930ee1d4b9d058d7
kobanyan <kobanyan@users.noreply.github.com>
2021-11-07 13:46:45 +0900

    fix infinite loop

--- functions/_tai_managed_by_asdf.fish
+++ functions/_tai_managed_by_asdf.fish
@@ -3,7 +3,7 @@ function _tai_managed_by_asdf -a tai_lang
   set -l __tool_versions_path (asdf current "$tai_lang" 2> /dev/null | string split -n ' ' -f 3)
   function __managed_by_asdf -a ___tool_versions_path
     set -l __path (string join / $argv[2..])
-    test "$__path" = "$HOME"; and return 1
+    test "$__path" = "$HOME" -o "$__path" = ""; and return 1
     test "$__path"/.tool-versions = "$___tool_versions_path";
       and return 0;
       or __managed_by_asdf "$___tool_versions_path" $argv[2..-2]
